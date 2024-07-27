777872223c31ea1268848cac672f34b6e337b653
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-07-27 21:03:46 +0200

    feat: header component

--- src/app/(landing)/_components/AboutUs/about-us.tsx
+++ src/app/(landing)/_components/AboutUs/about-us.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import HeaderSection from "@/components/header";
+
 const AboutUs = () => {
   return (
-    <div id="o-nama" className="flex h-screen items-center justify-center">
-      O nama
+    <div
+      id="o-nama"
+      className="flex h-screen flex-col items-center justify-start pt-28 md:pt-32"
+    >
+      <HeaderSection
+        title="O nama"
+        description="Ovo je sekcija o nama za pocetak"
+      />
     </div>
   );
 };
